7e37ac731abfc064b6bec4034c06abac015e9a8e
npenin <nicolas.penin@dragon-angel.fr>
2019-08-09 08:09:32 +0200

    moved to akala v3

--- src/client/tile.ts
+++ src/client/tile.ts
@@ -1,9 +1,11 @@
-import '@akala/client';
-import '@domojs/theme-default/dist/tile';
+///<reference types='@akala/client' />
+///<reference types="@domojs/theme-default/dist/tile" />
 
-
-tiles.add({
-    text: 'Evenements',
-    icon: 'bolt',
-    url: '/lifttt'
-});
+akala.module('@domojs/lifttt').run(['tiles'], function (tiles: tiles.TileService)
+{
+    tiles.add({
+        text: 'Evenements',
+        icon: 'bolt',
+        url: '/lifttt'
+    });
+})
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,35 +3,18 @@ import { AssetRegistration } from '@akala-modules/core';
 import { EventEmitter } from 'events';
 export * from './channel';
 
-akala.injectWithName(['$isModule', '$master', '$worker'], function (isModule: akala.worker.IsModule, master: akala.worker.MasterRegistration, worker: EventEmitter)
+const pkgName = require('../../package.json').name;
+
+akala.module(pkgName).init([AssetRegistration.name], async function (virtualasset: PromiseLike<AssetRegistration>)
 {
-    if (isModule('@domojs/lifttt'))
-    {
-        worker.on('ready', function ()
-        {
-            // Called when all modules have been initialized
-        });
-        worker.on('after-master', function ()
-        {
-            // Called when all modules have been initialized
-            require('./services/console');
-            require('./services/fs');
-            require('./services/http');
-            require('./api');
-        });
+    require('./services/console');
+    require('./services/fs');
+    require('./services/http');
+    require('./api');
 
+    require('./master');
 
-        master(__filename, './master');
-
-
-        akala.injectWithName([AssetRegistration.name], function (virtualasset: PromiseLike<AssetRegistration>)
-        {
-            virtualasset.then((va) =>
-            {
-                va.register('/js/tiles.js', require.resolve('../tile'));
-                va.register('/js/routes.js', require.resolve('../routes'));
-            });
-        })();
-
-    }
-})()
+    var va = await virtualasset;
+    va.register('/js/tiles.js', require.resolve('../tile'));
+    va.register('/js/routes.js', require.resolve('../routes'));
+});
